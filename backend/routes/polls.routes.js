0a5e19d4a9565fa1d21148d0d7174c2dd4583a17
Pranay <pmcanvas4501@gmail.com>
2023-12-08 10:27:02 +0530

    pagination for question api is added

--- backend/routes/polls.routes.js
+++ backend/routes/polls.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const pollRouter = express.Router();
-const { handleCreateNewPoll, handleGetAllPolls, handleUpdatePoll, handlePollsAnalaytics, handleSubmitForAPoll } = require('../controllers/poll.controller')
+const { handleCreateNewPoll, handleGetAllPolls, handleUpdatePoll, handlePollsAnalaytics, handleSubmitForAPoll, handleGetAllQuestionsForAPolls } = require('../controllers/poll.controller')
 
 pollRouter.post('/polls', handleCreateNewPoll)
 pollRouter.get('/polls', handleGetAllPolls)
+pollRouter.get('/polls/:pollId/questions', handleGetAllQuestionsForAPolls)
 pollRouter.put('/polls/:pollId', handleUpdatePoll)
 pollRouter.post('/polls/:pollId/submit', handleSubmitForAPoll)
 pollRouter.get('/polls/:pollId/analytics', handlePollsAnalaytics)
